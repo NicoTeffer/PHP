1d4b97ab5d579a3941bb37f05eaeea09fe0ffc5e
nicoteffer <n.teffer@gmail.com>
2023-08-17 22:36:07 +0200

    PHP & MySQL - Certification Course - added Lesson 26 - Forms in PHP

---
+++ Lesson26/forms.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Forms in PHP </title>
+</head>
+<body>
+
+<!-- CREATING A HTML FORM HERE -->
+<form action="send.php" method="post">
+    First Name: <input type="text" name="fname"><br>
+    E-mail: <input type="email" name="email"><br>
+    <input type="submit">
+</form>
+
+</body>
+</html>
---
+++ Lesson26/send.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Sending Forms in PHP </title>
+</head>
+<body>
+
+First Name: <?php echo $_POST["fname"]; ?><br>
+Email: <?php echo $_POST["email"]; ?><br>
+
+</body>
+</html>
