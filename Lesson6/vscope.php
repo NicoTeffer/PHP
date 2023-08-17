2045f537a2f64bddd4e28d9e1c61f71ad02d6e26
nicoteffer <n.teffer@gmail.com>
2023-08-17 14:33:23 +0200

    PHP & MySQL - Certification Course - added lesson 6 Variable Scope

---
+++ Lesson6/vscope.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Variable Scope </title>
+</head>
+<body>
+<?php
+
+    // GLOBAL SCOPE EXAMPLE
+
+    $x = 5; // global scope
+
+    function test1 () {
+        // Will generate an Error
+        echo "<p>Value of x is: $x</p>";
+    }
+    test1();
+
+    echo "<p>Value of x is: $x</p>";
+
+    // LOCAL SCOPE EXAMPLE
+
+    function test2 () {
+        $y = 5; // Local Scope
+        echo "<p>Value of y is: $y</p>";
+    }
+    test2();
+
+    // Will generate an Error
+    echo echo "<p>Value of y is: $y</p>";
+
+?>
+</body>
+</html>
