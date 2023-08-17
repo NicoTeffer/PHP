13d0d4e880c57b25024f64d437e83e92450d5ef6
nicoteffer <n.teffer@gmail.com>
2023-08-17 18:28:32 +0200

    PHP & MySQL - Certification Course - added Lesson 15 - Conditionals in PHP

---
+++ Lesson15/conditional.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Conditionals in PHP </title>
+</head>
+<body>
+<?php
+
+    # IF STATEMENT
+    $x = 55;
+    $y = 155;
+
+    if ($x < 50) {
+        echo "Condition met!";
+    }else if ($y > 100){
+        echo "Condition met!";
+    }else {
+        echo "Condition not met!";
+    }
+
+?>
+</body>
+</html>
