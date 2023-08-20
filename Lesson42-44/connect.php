a0dd7679ff62569a6befc35c3c7b830edc8678c2
nicoteffer <n.teffer@gmail.com>
2023-08-20 18:04:00 +0200

    PHP & MySQL - Certification Course - added Lesson 44 - SQL TABLE QUERY in PHP

---
+++ Lesson42-44/connect.php
@@ -0,0 +1,40 @@
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "Moogle050582";
+    // ADDED DB NAME
+    $dbname = "myWebsite";
+
+    // CREATE CONNECTION
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // CHECK CONNECTION
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    echo "Connected successfully!";
+    echo "<br>";
+    echo "<br>";
+
+    // SAVES QUERY IN A VARIABLE
+    $sql = "SELECT id, firstName, lastName, email, date FROM users";
+    // ESTABLISHED CONNECTION AND SENDS QUERY
+    $result = $conn->query($sql);
+
+    echo var_dump($result);
+    echo "<br>";
+    echo "<br>";
+
+    if ($result->num_rows > 0) {
+        // OUTPUT DATA IF THERE IS DATA IN ROWS QUERY
+        while ($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["id"] . " - Name: " . $row["firstName"] . " - Lastname: " . $row["lastName"] .
+            " - Email: " . $row["email"] . "<br>";
+        }
+    } else {
+        echo " 0 results!";
+    }
+
+    $conn->close();
